15ffd59fc2ebb7c5bae64efe6f62fdacb38762bb
oppahansi <oppa.hansi@gmx.de>
2014-12-22 12:33:07 +0100

    Initial commit

---
+++ src/de/oppahansi/episode3/HelloWorldApp.java
@@ -0,0 +1,10 @@
+package de.oppahansi.episode3;
+
+/*
+ * Created by oppahansi on 22.12.2014.
+ */
+public class HelloWorldApp {
+   public static void main(String[] args) {
+      System.out.println("Hello, World!");
+   }
+}
