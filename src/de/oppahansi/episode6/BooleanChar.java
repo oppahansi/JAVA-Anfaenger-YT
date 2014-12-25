3e486ebc42e075de06e25e3c3884f9d63d976bd7
oppahansi <oppa.hansi@gmx.de>
2014-12-25 18:04:05 +0100

    Episode 7 - byte, short, int und long

--- src/de/oppahansi/episode6/BooleanChar.java
+++ src/de/oppahansi/episode6/BooleanChar.java
@@ -28,14 +28,15 @@ public class BooleanChar {
       System.out.println("charTest hat den Wert: " + charTest);
       System.out.println("charLargest hat den Wert: " + charLargest);
 
-   }
-
    /*
     * Uebung 1:
     * Erstelle eine neue boolean Variable mit einem beliegen Namen und initialisiere
-    * dieser Variable mit dem Wert "true".
+    * diese Variable mit dem Wert "true".
     * (Optional: Gebe diese Variable in der Konsole aus.)
     */
+
+      boolean testBoolean = true;
+      System.out.println("testBoolean hat den Wert: " + testBoolean);
 
    /*
     * Uebung 2:
@@ -43,4 +44,10 @@ public class BooleanChar {
     * Zeichen.
     * (Optional: Gebe diese Variablen in der Konsole aus.)
     */
+
+      char initialeVorname = 'A';
+      char initialeNachname = 'S';
+      System.out.println("initialeVorname hat den Wert: " + initialeVorname);
+      System.out.println("intialeNachname hat den Wert: " + initialeNachname);
+   }
 }
