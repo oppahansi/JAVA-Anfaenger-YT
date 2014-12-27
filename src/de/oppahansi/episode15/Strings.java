3813972d7a1f65c9512c00c6dcac2eebd4df7531
oppahansi <oppa.hansi@gmx.de>
2014-12-28 00:37:06 +0100

    Episode 15 - String

---
+++ src/de/oppahansi/episode15/Strings.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Alexander Schellenberg alias oppahansi.
+ * Created for the JAVA Anfaenger Tutorial on Youtube.
+ */
+
+package de.oppahansi.episode15;
+
+public class Strings {
+
+   public static void main(String[] args) {
+      /**
+       * String - Zeichenkette
+       */
+      String helloWorld = "Hello, World!";
+
+      String vorname = "Oppa";
+      String nachname = "Hansi";
+
+      String vornameNachname = vorname + nachname;
+
+      System.out.println(helloWorld);
+      System.out.println("Mein Name ist " + vorname + " " + nachname);
+      System.out.println(vornameNachname);
+
+      /**
+       * Uebung 1:
+       * Verändere das Programm so, dass "Hello, World Oppa Hansi Tutorial"
+       * ausgegeben wird. Achte auf die Leerzeichen. Es gibt mehrere Loesungen
+       * fuer diese Uebung. Probiere es einfach aus.
+       */
+
+      /**
+       * Uebung 2:
+       * Kann man in der Zeile 19 anstatt + einen - Operator verwenden?
+       */
+   }
+}
