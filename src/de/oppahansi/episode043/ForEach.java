9c9ed31effad01f797e30c1d83e852cc6859dfd8
oppahansi <oppa.hansi@gmx.de>
2015-01-12 15:04:50 +0100

    Episode 43

---
+++ src/de/oppahansi/episode043/ForEach.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Alexander Schellenberg alias oppahansi.
+ * Created for the JAVA Anfaenger Tutorial on Youtube.
+ */
+
+package de.oppahansi.episode043;
+
+public class ForEach {
+   public static void main(String[] args) {
+      /**
+       * for-each loop - enhanced for-loop
+       *
+       * for (Datatype variable : array) {
+       *    // statements
+       * }
+       */
+
+      int[] testArray = {0, 5, 2, 8, 4, 1, 6, 7, 3, 9};
+
+      for (int arrayValue : testArray) {
+         System.out.println("arrayValue has the value -> " + arrayValue);
+      }
+   }
+}
